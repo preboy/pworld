cd06c031e718f5dc8d5314a146b7f90bb4a1199e
preboy.sl <preboy@126.com>
2018-04-10 17:36:10 +0800

    * script

--- test/test_connector.js
+++ test/test_connector.js
@@ -10,7 +10,7 @@ let err_cnt = 0;
 
 for( let i = 0; i < 1111; i++){
 
-    let c = net.createConnection(19850, "139.162.86.196", () => {
+    let c = net.createConnection(19850, "118.24.48.149", () => {
         sessions[sid] = new ss.Session(c, sid);
         sid++;
     });
